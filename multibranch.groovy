d68e0161d2b391a0b7cd1184820ef0ee34d298f9
Ray Ng <ray.ng@gradeous.com.au>
2020-11-03 17:08:56 +1100

    add multi branch project groovy

---
+++ multibranch.groovy
@@ -0,0 +1,27 @@
+String blueprintsFolder = 'RT-SRE/blueprints'
+UUID uuid = UUID.randomUUID()
+String folderName = [blueprintsFolder, lbu, appRef].join('/')
+multibranchPipelineJob("${folderName}") {
+    displayName "${appRef}"
+    description "{'appref': ${appRef}, 'purpose': 'terraform blueprint deployer', 'lbu': ${lbu}  }"
+    configure {
+        it / sources / 'data' / 'jenkins.branch.BranchSource' << {
+            source(class: 'jenkins.plugins.git.GitSCMSource') {
+                id(uuid)
+                remote(gitRepo)
+                credentialsId(repoCredential)
+                includes('*')
+                excludes('')
+                ignoreOnPushNotifications('false')
+                traits {
+                    'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
+                }
+            }
+        }
+        // customise the branch project factory
+        it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory") << {
+            // pipeline jobs will have their script path set to `pipelines/customPipeline.groovy`
+            scriptPath("Jenkinsfile")
+        }
+    }
+}
