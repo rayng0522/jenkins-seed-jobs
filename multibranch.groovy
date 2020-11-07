957dc1c8818755e1082e53747f67b3a728921a2a
Ray Ng <ray.ng@gradeous.com.au>
2020-11-08 01:48:45 +1100

    test

--- multibranch.groovy
+++ multibranch.groovy
@@ -28,14 +28,12 @@ jobs.each { job ->
             // }
 
             it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.extended.RemoteJenkinsFileWorkflowBranchProjectFactory") << {
-                setRemoteJenkinsFile("Jenkinsfile2")
+                remoteJenkinsFile("Jenkinsfile2")
                 remoteJenkinsFileSCM(class: 'jenkins.plugins.git.GitSCMSource') {
                    configVersion(2)
-                   it / userRemoteConfigs / 'hudson.plugins.git.UserRemoteConfig' << {
                     // it / sources / 'userRemoteConfigs' / 'hudson.plugins.git.UserRemoteConfig' << {
-                       url(gitRepo)
-                       credentialsId(repoCredential)
-                   }
+                   url(gitRepo)
+                   credentialsId(repoCredential)
                 }
             }
         }
