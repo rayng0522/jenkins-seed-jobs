0645d4e33ef4a3e9fe1595a5750055d83b3edda6
Ray Ng <ray.ng@gradeous.com.au>
2020-11-08 15:10:28 +1100

    test

--- multibranch.groovy
+++ multibranch.groovy
@@ -30,7 +30,7 @@ jobs.each { job ->
             it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.extended.RemoteJenkinsFileWorkflowBranchProjectFactory") << {                  scriptPath("Jenkinsfile")
                   localMarker()
                   remoteJenkinsFile("Jenkinsfile2")
-                  remoteJenkinsFileSCM(class: 'jenkins.plugins.git.GitSCMSource') {
+                  remoteJenkinsFileSCM(class: 'hudson.plugins.git.GitSCM') {
                      configVersion(2)
                       // it / sources / 'userRemoteConfigs' / 'hudson.plugins.git.UserRemoteConfig' << {
                      userRemoteConfigs { // This is the tag of type CONFIGURE
