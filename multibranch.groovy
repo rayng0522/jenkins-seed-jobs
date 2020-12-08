8fe2b6f996e638e3c07464ee03c17717b6a2f11e
Ray Ng <ray.ng@gradeous.com.au>
2020-12-08 15:21:57 +1100

    test commit

--- multibranch.groovy
+++ multibranch.groovy
@@ -1,10 +1,3 @@
-library identifier: 'jenkins-shared-libraries@tags/0.0.4', retriever: modernSCM([
-    $class: 'GitSCMSource',
-    remote: 'https://github.com/rayng0522/jenkins-shared-libraries.git',
-    credentialsId: '309ff56c-7993-4ed0-8003-dbdf15fb3f01'
-])
-
-
 jobs.each { job ->
     UUID uuid = UUID.randomUUID()
     String lbu        = job.adCode
