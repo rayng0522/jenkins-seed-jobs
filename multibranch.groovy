2c4f110ffb5cb3e19b3f3a2608cebf8a01f35a5d
Ray Ng <ray.ng@gradeous.com.au>
2020-12-09 12:48:19 +1100

    test commit

--- multibranch.groovy
+++ multibranch.groovy
@@ -1,5 +1,13 @@
-GroovyShell shell = new GroovyShell()
-def tools = shell.parse(new File('./emailNotification.groovy' ))
+def notify(status) {
+   wrap([$class: 'BuildUser']) {
+       emailext (
+       subject: "${status}: Job ${env.JOB_NAME} ([${env.BUILD_NUMBER})",
+       body: """
+       Check console output at <a href="${env.BUILD_URL}">${env.JOB_NAME} (${env.BUILD_NUMBER})</a>""",
+       to: "${BUILD_USER_EMAIL}",
+       from: 'jenkins@company.com')
+   }
+}
 
 jobs.each { job ->
     UUID uuid = UUID.randomUUID()
