5c5978200bf40a8555ad4474b03416b81980fcf1
Ray Ng <ray.ng@gradeous.com.au>
2020-12-09 12:33:20 +1100

    test commit

--- multibranch.groovy
+++ multibranch.groovy
@@ -1,23 +1,4 @@
-import javax.mail.*
-import javax.mail.internet.*
-
-def sendMail(host, sender, receivers, subject, text) {
-    Properties props = System.getProperties()
-    props.put("mail.smtp.host", host)
-    Session session = Session.getDefaultInstance(props, null)
-
-    MimeMessage message = new MimeMessage(session)
-    message.setFrom(new InternetAddress(sender))
-    receivers.split(',').each {
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(it))
-    }
-    message.setSubject(subject)
-    message.setText(text)
-
-    println 'Sending mail to ' + receivers + '.'
-    Transport.send(message)
-    println 'Mail sent.'
-}
+@Library("jenkins-shared-libraries@tags/0.0.4")
 
 jobs.each { job ->
     UUID uuid = UUID.randomUUID()
